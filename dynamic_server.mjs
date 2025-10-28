d6d0fb47834b26f4e5d0d0d517c555195d5f779b
Mark DeSutter <desu5994@stthomas.edu>
2025-10-28 13:11:36 -0500

    fixed issues in temperature.html and added dynamic table

--- dynamic_server.mjs
+++ dynamic_server.mjs
@@ -23,6 +23,38 @@ const db = new sqlite3.Database('./KMDW.sqlite3', sqlite3.OPEN_READONLY, (err) =
     }
 });
 
+app.get('/', (req, res) => {
+    fs.readFile(path.join(template, 'index.html'), {encoding: 'utf8'}, (err, data) =>{
+        if(err){
+            res.status(404).send('Page Not Found 404');
+        }
+        else{
+            res.status(200).type('html').send(data);
+        }
+    });
+});
+
+app.get('/temperature.html', (req, res) => {
+    db.all('SELECT date, actual_mean_temp, actual_min_temp, actual_max_temp FROM KMDW', (err, rows) => {
+        if(err){
+            res.status(500).type('txt').send('SQL Error');
+        }
+        else{
+            fs.readFile(path.join(template, 'temperature.html'), {encoding: 'utf8'}, (err, data) => {
+                let tr_string = '';
+                for(let i = 0; i < rows.length; i++){
+                    tr_string += '<tr><td>' + rows[i].date + '</td>';
+                    tr_string += '<td>' + rows[i].actual_mean_temp + '</td>';
+                    tr_string += '<td>' + rows[i].actual_min_temp + '</td>';
+                    tr_string += '<td>' + rows[i].actual_max_temp + '</td></tr>';
+                }
+                let response = data.replace('$$$TEMPERATURE_ROWS$$$', tr_string);
+                res.status(200).type('html').send(response);
+            });
+        }
+    });
+});
+
 app.listen(port, () => {
     console.log('Now listening on port ' + port);
 });
